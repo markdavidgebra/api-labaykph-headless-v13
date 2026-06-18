c2e3b567b7ce21e08b6811f2869c812f0ce97936
Mark David Billena <markdavidgebra@gmail.com>
2026-06-18 10:39:40 +0800

    Update the Cors

--- config/cors.php
+++ config/cors.php
@@ -1,18 +1,31 @@
 <?php
 
+$extraOrigins = array_map('trim', explode(',', (string) env('CORS_ALLOWED_ORIGINS', '')));
+
 return [
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => array_filter([
-        env('FRONTEND_URL', 'http://localhost:5173'),
-        'http://localhost:5173',
-        'http://127.0.0.1:5173',
-    ]),
+    'allowed_origins' => array_values(array_unique(array_filter(array_merge(
+        [
+            'http://localhost:5173',
+            'http://127.0.0.1:5173',
+            'https://labaykph.com',
+            'https://www.labaykph.com',
+            'https://test.labaykph.com',
+        ],
+        [
+            env('FRONTEND_URL'),
+            env('APP_URL'),
+        ],
+        $extraOrigins,
+    )))),
 
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => [
+        '#^https://([a-z0-9-]+\.)?labaykph\.com$#',
+    ],
 
     'allowed_headers' => ['*'],
 
--- config/sanctum.php
+++ config/sanctum.php
@@ -18,11 +18,18 @@ return [
     |
     */
 
-    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
-        '%s%s',
-        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
-        Sanctum::currentApplicationUrlWithPort(),
-        // Sanctum::currentRequestHost(),
+    'stateful' => array_values(array_unique(array_filter(array_map(
+        'trim',
+        explode(',', env('SANCTUM_STATEFUL_DOMAINS', implode(',', [
+            'localhost',
+            'localhost:5173',
+            '127.0.0.1',
+            '127.0.0.1:5173',
+            'test.labaykph.com',
+            'labaykph.com',
+            'www.labaykph.com',
+            Sanctum::currentApplicationUrlWithPort(),
+        ])))
     ))),
 
     /*
